838e2545b1f7c16e07228fe3de03caf51d4c77f4
inf00248 <george.martins@terceiro-sky.com.br>
2022-02-25 16:56:15 -0300

    Initial Commit - Adding structure

---
+++ src/schema/BlingSchema.js
@@ -0,0 +1,13 @@
+export const json =  (item) => `
+{
+    "cliente": {
+       "nome": "Ricardo"
+    },
+    "item": {
+       "codigo": "00052",
+       "descricao": "Caderno 002",
+       "vlr_unit": "3.75",
+       "qtde": "10"
+    }
+ }
+`;
