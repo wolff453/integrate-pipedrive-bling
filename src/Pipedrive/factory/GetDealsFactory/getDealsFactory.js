193ff1b779362589157699fc0ebf3fd993cf7dc4
wolff453 <george8234@hotmail.com>
2022-02-28 16:32:20 -0300

    feat: add factory

---
+++ src/Pipedrive/factory/GetDealsFactory/getDealsFactory.js
@@ -0,0 +1,12 @@
+import { PipedriveRepository } from "../../repository/pipedriveRepository.js"
+import { PipedriveService } from '../../services/PipedriveService.js'
+import { GetController } from "../../controllers/GetController/GetController.js"
+import config from '../../../config/index.js'
+import mongo from "../../../common/lib/mongo/index.js"
+
+export const getDealsPipedriveFactory = () => {
+    const pipedriveRepository = PipedriveRepository({ mongo })
+    const pipedriveService = PipedriveService({ config, pipedriveRepository })
+    const pipedriveController = GetController({ pipedriveService })
+    return pipedriveController.handle
+}
