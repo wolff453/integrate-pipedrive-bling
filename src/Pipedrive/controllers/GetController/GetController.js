a06cab3871ea69cdd38aea4aa8fdf92d750cae89
wolff453 <george8234@hotmail.com>
2022-02-28 16:31:54 -0300

    feat: add route controllers

---
+++ src/Pipedrive/controllers/GetController/GetController.js
@@ -0,0 +1,10 @@
+export const GetController = ({ pipedriveService }) => ({
+    handle: async (request, response) => {
+        try {
+            const deals = await pipedriveService.getBlingDealsFromDatabase()
+            return response.status(200).send(deals)
+        } catch (error) {
+            return response.status(500).send({ message: error.message })
+        }
+    }
+})
---
+++ src/Pipedrive/controllers/PostController/PostController.js
@@ -0,0 +1,12 @@
+export const PostController = ({ pipedriveService }) => ({
+    handle: async (request, response) => {
+        try {
+            const deals = await pipedriveService.getAllWonStatus()
+            await pipedriveService.insertDealsInBling(deals)
+            return response.status(200).send({ message: 'Data entered succesfully' })
+        } catch (error) {
+            return response.status(error.response.status || 500).send({ message: error.message })
+
+        }
+    }
+})
