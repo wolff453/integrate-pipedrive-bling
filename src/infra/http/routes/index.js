c58e1cad1c5a50f3a9b942da12fe3b8f1f228f82
George Lobo <77250912+wolff453@users.noreply.github.com>
2022-03-02 10:22:17 -0300

    Update index.js

--- src/infra/http/routes/index.js
+++ src/infra/http/routes/index.js
@@ -8,4 +8,4 @@ const app = express()
 
 app.use(pipedrive)
 
-app.listen(config.infra.port, () => console.log('Rodando na porta 3000'))
+app.listen(config.infra.port, () => console.log(`Rodando na porta ${config.infra.port}`))
